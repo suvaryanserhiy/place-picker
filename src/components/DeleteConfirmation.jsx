d75912088292f95e8d9b47ae644635795b6b4d27
SSO <sso@d.co>
2025-05-04 20:40:04 +0200

    added progress bar to modal dialog

--- src/components/DeleteConfirmation.jsx
+++ src/components/DeleteConfirmation.jsx
@@ -1,6 +1,17 @@
-import { useEffect } from 'react';
-
+import { useEffect, useState } from 'react';
+const TIMER = 3000;
 export default function DeleteConfirmation({ onConfirm, onCancel }) {
+	const [remainingTime, setRemainingTime] = useState(TIMER);
+
+	useEffect(() => {
+		const interval = setInterval(() => {
+			setRemainingTime((prevTime) => prevTime - 10);
+		}, 10);
+    return () => {
+			clearInterval(interval);
+		};
+	}, []);
+
 	useEffect(() => {
 		const timer = setTimeout(() => {
 			onConfirm();
@@ -23,6 +34,7 @@ export default function DeleteConfirmation({ onConfirm, onCancel }) {
 					Yes
 				</button>
 			</div>
+			<progress value={remainingTime} max={TIMER} />
 		</div>
 	);
 }
